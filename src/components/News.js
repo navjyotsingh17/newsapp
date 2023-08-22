081cb7594da77748687b75b528782c90b7feba7a
navjyotsingh17 <navjyotpulaha@gmail.com>
2023-08-22 13:16:18 +0530

    News API version

--- src/components/News.js
+++ src/components/News.js
@@ -12,7 +12,7 @@ const News = (props) => {
     const [articles, setArticles] = useState([])
     const [loading, setLoading] = useState(true)
     const [page, setPage] = useState(1)
-    const [totalResults, setTotalResults] = useState(0)
+    const [totalarticles, setTotalarticles] = useState(0)
 
     //a function to capitalize the first letter of a string
     const capitalizeFirstLetter = (string) => {
@@ -23,14 +23,14 @@ const News = (props) => {
     const updateNews = async () => {
         props.setProgress(10);
         // in the below url value i have enterd back ticks so that i can write java script in it.
-        const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page}&pageSize=${props.pageSize}`;
+         const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page}&pageSize=${props.pageSize}`;
         setLoading(true)
         let data = await fetch(url); // here we have to write await because its a async fuction this returns a promise so we have to wait for this promise to get resolved
         props.setProgress(30);
         let parsedData = await data.json()
         props.setProgress(70);
         setArticles(parsedData.articles)
-        setTotalResults(parsedData.totalResults)
+        setTotalarticles(parsedData.totalarticles)
         setLoading(false)
         props.setProgress(100);
 
@@ -38,20 +38,22 @@ const News = (props) => {
 
     // use effect method whenever the page changes it changes the document title based on the category and also updateNews method is called to render the news based on the news props category
     useEffect(() => {
-        document.title = `${capitalizeFirstLetter(props.category)} - NewsMonkey`;
+        document.title = `${capitalizeFirstLetter(props.category)} - NewsMonkey | Get your daily dose of news for free!`;
         updateNews();
         // eslint-disable-next-line
     }, []) // By providing an empty dependency array [], you are telling React that the effect has no dependencies and should only run once, when the component is initially rendered
 
     // fetchMoreData is used for the infinite scroll feature so that when the user scrolls at the bottom of the page this method is called which concates more news with exsiting news on that page.
+    
     const fetchMoreData = async () => {
         const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page + 1}&pageSize=${props.pageSize}`;
-        setPage(page + 1)
+        setPage(page + 1)                            // to resolve the unique key value problem i am setting the page to page+1 in the url becoz the setPage state below the url takes some mili seconds to resolve and before that my url is getting made, so have maually setting the page stae as page + 1 in the url and after that as well
         let data = await fetch(url);
         let parsedData = await data.json()
         setArticles(articles.concat(parsedData.articles))
-        setTotalResults(parsedData.totalResults)
+        setTotalarticles(parsedData.totalarticles)
     };
+
 
     return (
         <>
@@ -61,11 +63,11 @@ const News = (props) => {
             {/* here i am showing the spinner gif based on the loading flag if it's true or false */}
             {loading && <Spinner />}
 
-            {/* here in the InfiniteScroll tag i am setting the dataLength according to the articles parameter from the news API json response, then calling the fetchMoreData fuction to load more news, setting the hasMore variable comparing the articles parameter length from the API json reponse withe totalResults state */}
+            {/* here in the InfiniteScroll tag i am setting the dataLength according to the articles parameter from the news API json response, then calling the fetchMoreData fuction to load more news, setting the hasMore variable comparing the articles parameter length from the API json reponse withe totalarticles state */}
             <InfiniteScroll
                 dataLength={articles.length}
                 next={fetchMoreData}
-                hasMore={articles.length !== totalResults}
+                hasMore={articles.length !== totalarticles}
                 loader={<Spinner />}
             >
                 <div className="container">
